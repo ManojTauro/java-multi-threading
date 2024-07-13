bddb1b0f9af1d04d5d9a6f0e314de121e92dc6e7
manu2931 <manojb912@gmai.com>
2024-07-13 10:37:43 +0100

    Implemented MatrixGenerator to generate 100000 matrix pairs

---
+++ MatrixProducerConsumer/src/MatrixGenerator.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MatrixGenerator {
+    private static final String OUTPUT_FILE = "./out/matrices";
+    private static final int size = 10;
+    private static final int NUMBER_OF_MATRIX_PAIRS = 100000;
+    private static Random random = new Random();
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(OUTPUT_FILE);
+        try (FileWriter writer = new FileWriter(file)) {
+            createMatrices(writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createMatrices(FileWriter writer) throws IOException {
+        for (int i = 0; i < NUMBER_OF_MATRIX_PAIRS; i++) {
+            int[][] matrix = createMatrix();
+            saveMatrix(writer, matrix);
+        }
+    }
+
+    private static int[][] createMatrix() {
+        return IntStream.range(0, size)
+                .mapToObj(r -> createRow())
+                .toArray(int[][]::new);
+
+    }
+
+    private static int[] createRow() {
+        return random.ints(size, 0, 100).toArray();
+    }
+
+    private static void saveMatrix(FileWriter writer, int[][] matrix) throws IOException {
+            for (int[] row: matrix) {
+                String rowString = IntStream.of(row)
+                        .mapToObj(Integer::toString)
+                        .reduce((a, b) -> a+","+b)
+                        .orElse("");
+
+                writer.write(rowString);
+                writer.write('\n');
+            }
+
+            writer.write('\n');
+    }
+}
